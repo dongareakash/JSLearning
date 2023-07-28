e237df7c089fc525d805bf675c2193ea51614163
dongareakash <akashbdongare99@gmail.com>
2023-07-28 15:51:49 +0530

    ifblock
    ifblock

---
+++ 06_ifblock.js
@@ -0,0 +1,18 @@
+console.log("-----------------Assignment 1-------------------");
+function num(num) {
+
+    if (num%2==0) {
+        console.log(`${num} given number is even`);
+    } else {
+        console.log(`${num} given number is odd`);
+    }
+    
+}
+num(45);
+num(70);
+num(67);
+num(98);
+
+
+
+
