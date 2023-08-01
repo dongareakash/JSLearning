1c7a1408831915d4b4c5af1a60d406873b79680f
dongareakash <akashbdongare99@gmail.com>
2023-08-01 20:19:07 +0530

    whileloop
    whileloop

---
+++ 06_whileLoop.js
@@ -0,0 +1,72 @@
+var i = 1
+while (i<= 10) {
+    console.log(i);
+    i++;
+}
+console.log("--------------------------------");
+//1.WAP tp print numbuer from 5 to 15 by incrementing 1
+var i = 5
+while (i <= 15) {
+    console.log(i);
+    i++;
+}
+console.log("--------------------------------");
+
+//2.WAP tp print number from 50 to 40 by decrementing 1
+var i = 50
+while (i >= 40) {
+    console.log(i);
+    i--;
+}
+console.log("--------------------------------");
+
+//3.WAP to find first 15 odd number
+var i= 1;
+while (i <=30) {
+    console.log(i);
+    i=i+2;
+}
+console.log("--------------------------------");
+
+//4.WAP to find first 10 even number
+var i= 0;
+while (i< 20) {
+    console.log(i);
+    i=i+2;
+    
+}
+console.log("--------------------------------");
+
+//5.WAP to print table of 5 like --> 5 10 15 20 25 ... 50
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+var i = 5;
+while (i<= 50) {
+    console.log(i);
+    i=i+5;
+}
+console.log("--------------------------------");
+
+//6.WAP to print table of 10 like 10 20 30 40 .... 100
+var i=10;
+while (i< 100) {
+    console.log(i);
+    i=i+10;
+}
+console.log("--------------------------------");
+
+//7.WAP to print table of 10 reverse order like 100 90 80 70 ... 10
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+    
+}
+console.log("--------------------------------");
+
+var i= 100;
+while (i>= 10) {
+    console.log(i);
+    i=i-10;
+}
+console.log("--------------------------------");
